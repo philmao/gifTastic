b7ca7e0dd75f0d27dbe072663b5bd9a199019bdf
philmao <philip_mao@yahoo.com>
2017-05-19 22:11:43 -0700

    add some style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,8 @@ var topics = [ "dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "
 var gifTastic = {
 	maxLimit: 10,
 	renderButtons: function() {
-		console.log('renderButtons called');
-		// $("#animal-view").empty();
+
+		$("#topic-view").empty();
 
 	    for (var i = 0; i < topics.length; i++) {
 
@@ -13,32 +13,23 @@ var gifTastic = {
 			a.addClass("topic");
 			a.attr("data-name", topics[i]);
 			a.text(topics[i]);
-			$("#animal-view").append(a);
+			$("#topic-view").append(a);
 
 		}
+	},
+	addTopic: function() {
+		var newTopic = $('#topic-input').val();
+		topics.push(newTopic);
 	}
 }
 
-// for ( var i = 0; i < topics.length; i++ ) {
-// 	$('#animal-view').append('<button>' + topics[i] + '</button>');
-// 	console.log(topics[i]);
-// }
 
+$("#add-topic").on("click", function(event) {
 
-
-$("#add-animal").on("click", function(event) {
-  // event.preventDefault() prevents submit button from trying to send a form.
-  // Using a submit button instead of a regular button allows the user to hit
-  // "Enter" instead of clicking the button if desired
   event.preventDefault();
 
-  var newAnimal = $('#animal-input').val();
-  topics.push(newAnimal);
+  gifTastic.addTopic();
 
-  // Write code to grab the text the user types into the input field
-  // Write code to add the new movie into the movies array
-
-  // The renderButtons function is called, rendering the list of movie buttons
   gifTastic.renderButtons();
 });
 
